a0121c438da7b5130b9b6848d4f013ccdd8eebc9
jacbart <jacbart@gmail.com>
2017-06-28 19:47:07 +0000

    Created a foreign key for resources to connect to topics; Changed engine to InnoDB; Reformatted the way primary and foreign keys are created

--- SQL/db.sql
+++ SQL/db.sql
@@ -5,22 +5,20 @@ set autocommit=0;
 
 drop table if exists `users`;
 create table `users` (
-	`id` int(1) not null AUTO_INCREMENT,
-	`name` varchar(20) not null default '',
-	primary key (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=5;
+	`id` int(1) not null AUTO_INCREMENT primary key,
+	`name` varchar(20) not null default ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=5;
 insert into `users` (`id`, `name`) values
 	(1, 'admin'),
-	(2, 'anotheradmin'),
-	(3, 'yepagain'),
-	(4, 'yeahadminagainsuckit');
+	(2, 'user1'),
+	(3, 'user2'),
+	(4, 'user3');
 
 drop table if exists `topics`;
 create table `topics` (
-	`id` int(1) not null AUTO_INCREMENT,
-	`topic` varchar(15) not null default '',
-	primary key (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=4;
+	`id` int(1) not null AUTO_INCREMENT primary key,
+	`topic` varchar(15) not null default ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=4;
 insert into `topics` (`id`, `topic`) values
 	(1, 'python'),
 	(2, 'cpp'),
@@ -28,13 +26,12 @@ insert into `topics` (`id`, `topic`) values
 
 drop table if exists `resources`;
 create table `resources` (
-	`id` int(1) not null AUTO_INCREMENT, 
-	`topic_id` int(1) not null,
+	`id` int(1) not null AUTO_INCREMENT primary key, 
 	`title` varchar(40) not null default '',
 	`resource` varchar(200) not null default '',
-	primary key (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=4;
-insert into `resources` (`id`, `topic_id`, `title`, `resource`) values
-	(1, 1, 'ultimate python guide', 'https://www.google.com/search?q=pyhone&oq=pyhone&aqs=chrome..69i57j0l5.3420j0j7&sourceid=chrome&ie=UTF-8#q=python'),
-	(2, 2, 'ultimate cpp guide', 'https://www.google.com/search?q=pyhone&oq=pyhone&aqs=chrome..69i57j0l5.3420j0j7&sourceid=chrome&ie=UTF-8#q=c%2B%2B'),
-	(3, 3, 'google', 'https://support.google.com/websearch/answer/134479?hl=en');
+	`topic_id` int, foreign key (`topic_id`) references `topics`(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=4;
+insert into `resources` (`id`, `title`, `resource`, `topic_id`) values
+	(1, 'ultimate python guide', 'https://www.google.com/search?q=pyhone&oq=pyhone&aqs=chrome..69i57j0l5.3420j0j7&sourceid=chrome&ie=UTF-8#q=python', 1),
+	(2, 'ultimate cpp guide', 'https://www.google.com/search?q=pyhone&oq=pyhone&aqs=chrome..69i57j0l5.3420j0j7&sourceid=chrome&ie=UTF-8#q=c%2B%2B', 2),
+	(3, 'google', 'https://support.google.com/websearch/answer/134479?hl=en', 3);
