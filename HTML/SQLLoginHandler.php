932c5127045b9c2d5da117831f00aca0bac278f7
jacbart <jacbart@gmail.com>
2017-07-03 17:31:32 +0000

    Changed variable name to make more sense

--- HTML/SQLLoginHandler.php
+++ HTML/SQLLoginHandler.php
@@ -21,12 +21,12 @@ if (!$_POST['username']){
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["host"];
-$username2 = $url["user"];
+$username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
 // Connects to your Database 
-$connection = mysqli_connect($server,$username2,$password,$db);
+$connection = mysqli_connect($server,$username,$password,$db);
 // $connection = mysqli_connect("localhost","root","1080", "ThatCSGuide");
 if (!$connection) {
     echo "<h4>Failed to connect to MySQL: ".mysqli_connect_error();
