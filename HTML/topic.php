fe4257d495bf4f6dd371e08f8fe45a30622dab0c
jacbart <jacbart@gmail.com>
2017-07-05 23:04:58 +0000

    Testing dynamic topic.php

--- HTML/topic.php
+++ HTML/topic.php
@@ -17,8 +17,7 @@ if(!$connect)
 
 // Gets topic id
 $topicid = $_REQUEST['topicid'];
-$topicid = (int)$topicid;
-echo $topicid;
+// $topicid = (int)$topicid;
 $topicQuery = "select * from topics where id=".$topicid.";";
 $topicResult = mysqli_query($connect, $topicQuery);
 if(!$topicResult)
