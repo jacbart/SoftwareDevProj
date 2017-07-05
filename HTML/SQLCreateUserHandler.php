f4d511fa2237cacfce9874b6fa82b6a8b0d4f8bd
Holden Sheftel <hosh6209@colorado.edu>
2017-07-05 17:54:05 -0600

    Refactored code

--- HTML/SQLCreateUserHandler.php
+++ HTML/SQLCreateUserHandler.php
@@ -1,7 +1,16 @@
 <?php
+// Check if new username contains only valid characters
 // adapted from https://stackoverflow.com/questions/1735972/php-fastest-way-to-check-for-invalid-characters-all-but-a-z-a-z-0-9
 function isValid($username) {
 	return !preg_match('/[^a-z0-9.\-_]/i', $username);
+}
+
+// Check if username is taken
+function isFreeUsername($username, $connection) {
+	$namecheckquery = mysqli_query($connection, "select name from users where name = '".$username."';");
+	$namecheck = mysqli_num_rows($namecheckquery);
+
+	return !$namecheck;
 }
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
@@ -22,12 +31,10 @@ if (!$connection) {
 
 $usr = $_REQUEST['username'];
 
-$namecheckquery = mysqli_query($connection, "select name from users where name = '".$usr."';");
-$namecheck = mysqli_num_rows($namecheckquery);
 if (!isValid($usr)) {
 	echo "<p>Invalid username</p>";
 }
-elseif ($namecheck) {
+elseif (!isFreeUsername($usr,$connection)) {
 	echo "<p>Username taken</p>";
 }
 else {
