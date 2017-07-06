4577bee171ceb98f2e8767226433abc1d62b89e8
Holden Sheftel <hosh6209@colorado.edu>
2017-07-06 14:29:59 -0600

    test

--- HTML/favorites.php
+++ HTML/favorites.php
@@ -137,15 +137,14 @@ while ($topicRow = mysqli_fetch_array($topicResult))
                         die('Could query data: '.mysqli_error($connect).' because '.mysqli_errno($connect));
                     }
 					$favs = explode(',', $favresult);
-					echo $favs;
 					foreach ($favs as &$val) {
 						$val = (int)$val;
 					}
-					echo $favs;
                     // Selects the rows the have the matching topic id of topicResults[0] and displays them
                     while ($row = mysqli_fetch_array($resresult))
                     {
-						echo $row[0]." ".$row['id']."<br>";
+						//echo $row[0]." ".$row['id']."<br>";
+						echo $favs;
                         if(in_array($row['id'], $favs))
                         {
 							echo "
